6574b6b53d23797a2e88e564478cc82a8aa6ba19
Rajiv <youther.biswas8@gmail.com>
2017-08-20 12:08:17 +0530

    Update main.c

--- ADCReadInterrupts/main.c
+++ ADCReadInterrupts/main.c
@@ -1,3 +1,70 @@
+/* ------------------------------------------------------------------------------------------------------------------------------
+    main.c - main functions.
+    Copyright (C) 2017 Rajiv Biswas <rajiv.biswas55@gmail.com>
+
+    The Program here uses ADC10, 10-Bit Internal ADC Interrupt of TI 
+    MSP430G2553 to sample Analogue values in the Interrupt routine,
+    
+    __interrupt void ADC10ISR (void)
+    {
+    ADC10CTL0 &= ~ENC;
+    EQVal1[0]   = ADC10MEM;
+    val1 = ( EQVal1[0] >> 8 ) & 0x03;
+    val2 = EQVal1[0] & 0xff;
+
+    uart_send_ADCvalue( val2, val1 );
+    }
+    
+    The EQVal1 array is an Unsigned Integer array of size 1 (4-Bytes) on
+    TI MSP430GXX platform. But since the ADC value would be in 10-bit, 1023
+    Max, hence we need to consider only 10 bits from the Least significant 
+    Bit. Thus, two unsigned byte variables val1 & val2 are considered here. 
+    val2 - stores the least 8-bits of the ADC Sample
+    val1 - stores the last 2-bits of the ADC Sample
+    
+    The ADC Channel that has been configured here is ADC Channel, A5, P1.5
+    
+    ADC10AE0  = BIT5;                            // Select channel A5, P1.5
+    ADC10CTL0 = ADC10ON + REFON + ADC10SR + SREF0 + ADC10SHT_3 + ADC10IE;
+    ADC10CTL1 = INCH0 + INCH2 + ADC10DIV0 +ADC10DIV1 + CONSEQ_2;
+    ADC10CTL0 |= ADC10SC + ENC;                  // Start conversion
+    
+    Please be careful not to select ADC10 Analog Input A1 & ADC10 Analog 
+    Input A2, as we are using it for UART Tx & Rx.
+    
+    From the ADC Interrupt routine we call the uart_send_ADCvalue routine,
+    which Unpacks the values in neat ASCII Formatted Data Arrays & then 
+    triggers the Tx, Transmit Interrupt, USCIAB0TX_VECTOR associated with 
+    USCI_A0.
+    
+    The Program has been compiled & built on 
+    Code Composer Studio 
+
+    Version: 7.1.0.00016  
+    
+    Connect your Analog Input +ve  to A5, P1.5 & -ve to the Gnd of your TI 
+    MSP 430G2 LaunchPad Board & connect a FTDI Serial UART Cable or PL203
+    with Rx/Tx connected to P1.1(Rx), P1.2(Tx), with Gnd & Vcc connected to
+    corresponding Vcc & Gnd of the TI MSP430G Launchpad Board.
+    
+    Also please ensure to connect the RxD & TxDJumpers in Vertical Position,
+    & not in Horizontal Position, as shown in the attached png file.
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   ----------------------------------------------------------------------------------------------------------------------------- */
+
 #include <msp430.h> 
 
 /*
